d11ec6d31cc46ddc623e326b035881ac28817e14
lutyoung <ghazallutfulmannan@gmail.com>
2021-10-28 01:54:28 +0100

    Completed ElectionService by changing database model to dto

--- VotingViews/DTOs/UpdateElectionDto.cs
+++ VotingViews/DTOs/UpdateElectionDto.cs
@@ -10,5 +10,10 @@ namespace VotingViews.DTOs
         public string Name { get; set; }
 
         public string Status { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
     }
 }
